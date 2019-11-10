922c5a47b95cdae0b80f94b54885cd387bbb8601
jonra1993 <jon_ra@hotmail.es>
2019-11-10 17:31:35 -0500

    Set gpio

--- player.py
+++ player.py
@@ -4,26 +4,25 @@ from time import sleep
 import os
 import RPi.GPIO as GPIO
  
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM)      # set up BCM GPIO numbering 
 GPIO.setwarnings(False)
-GPIO.setup(17, GPIO.IN)
-GPIO.setup(18, GPIO.IN)
+button = 4
+GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)     # set GPIO21 as input and interl pull up is activated(button) 
  
 print("------------------")
 print(" OMXPlayer + GPIO ")
 print("------------------")
  
 print("--------------")
-print GPIO.input(17)
-print GPIO.input(18)
+print(GPIO.input(button))
 print("--------------")
-while True:
-        if ( GPIO.input(17) == False ):
+
+try:
+    while True:
+        if ( GPIO.input(button) == False ): # Button was pressed
             print("Button Pressed")
             os.system('omxplayer -o local ni.mp3 &')
-            sleep(3)
-     
-        if ( GPIO.input(18) == False ):
-            print("Button Pressed")
-            os.system('omxplayer -o hdmi intro.mp4 &')
             sleep(15)
+except KeyboardInterrupt:
+    pass
+        
